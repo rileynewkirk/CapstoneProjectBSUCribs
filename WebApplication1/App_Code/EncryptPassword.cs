37baaf5ce98806b74c903bd58bffb1b44bb3f0b4
nathan barr <nbarr013@gmail.com>
2017-10-23 12:18:27 -0700

    added styling sheets and login backend template

---
+++ WebApplication1/App_Code/EncryptPassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace WebApplication1.App_Code
+{
+    public class EncryptPassword
+    {
+        public static string encryptString(string input)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(input));
+            byte[] result = md5.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+            return strBuilder.ToString();
+        }
+    }
+}

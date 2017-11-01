1433bc3f934206ccec2b598066d01320e28b2d21
nathan barr <nbarr013@gmail.com>
2017-10-31 19:50:02 -0700

    registration works, needs login work

--- WebApplication1/Registration.aspx.cs
+++ WebApplication1/Registration.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using WebApplication1.App_Code;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +13,49 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+        protected void clear(object sender, EventArgs e)
+        {
+            tbUsername.Text = "";
+            tbPassword2.Text = "";
+            tbPassword.Text = "";
+            tbLastName.Text = "";
+            tbFirstName.Text = "";
+            tbEmail.Text = "";
+            tbPhoneNumber.Text = "";
+        }
+
+        protected void addUser(object sender, EventArgs e)
+        {
+            UserUtil user = new UserUtil();
+            DateTime dt = DateTime.Now;
+
+            user.UserName = tbUsername.Text;
+            user.firstname = tbFirstName.Text;
+            user.lastname = tbLastName.Text;
+            user.email = tbEmail.Text;
+            user.password = tbPassword.Text;
+            user.phoneNumber =tbPhoneNumber.Text;
+            user.UserType = 1;
+
+
+
+            if (!user.checkUserExist())
+            {
+                user.insertdata();
+                lblResponse.Text = "You have registered successfully! You will be redirect to the Login page momentarily";
+                lblResponse.ForeColor = Color.Green;
+                Response.AddHeader("refresh", "3;url=Login.aspx");
+
+            }
+            else
+            {
+                lblResponse.Text = "The username already exists";
+                lblResponse.ForeColor = Color.Red;
+                tbPassword2.Text = "";
+                tbPassword.Text = "";
+            }
 
         }
     }
